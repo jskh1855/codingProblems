4a514ca8abc31e15429cccaa6117357ea6460213
김형석 <70968732+kim-hyeungsuk@users.noreply.github.com>
2022-11-24 00:57:24 +0900

    solution for Flags

---
+++ codility/Flags.py
@@ -0,0 +1,44 @@
+def solution(A):
+    
+    peaks = []
+    answer = 0
+
+    for i in range(1,len(A)-1):
+
+        if A[i-1] < A[i] and A[i] > A[i+1]:
+
+            peaks.append(i)
+
+    if len(peaks) < 3:
+
+        return len(peaks)
+
+    flags = min(int(len(A)**(0.5))+1, len(peaks))
+
+    while flags > 2:
+
+        cur_flags = flags-1
+        cur_max = 1
+        prev = 0
+
+        for i in range(1,len(peaks)):
+
+            if cur_flags == 0:
+
+                break
+
+            if peaks[i]-peaks[prev] >= flags:
+
+                cur_max += 1
+                cur_flags -= 1
+                prev = i
+
+        if cur_max >= answer:
+
+            answer = cur_max
+            flags -= 1
+
+        else:
+            break
+    
+    return answer
